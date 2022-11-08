6d5744329d449863607a75bb5b6d8c16f4af0f0f
ikun2019 <0215.miyatto@gmail.com>
2022-11-09 04:42:16 +0900

    middlewareの作成

--- src/api/post/policies/check-role.js
+++ src/api/post/policies/check-role.js
@@ -7,8 +7,8 @@
 module.exports = (policyContext, config, { strapi }) => {
   // Add your own logic here.
   const { userRole } = config;
-
-  const isEligible = policyContext.state.user && policyContext.state.user.role.code == userRole;
+  // console.log(policyContext.state.user.role.name);
+  const isEligible = policyContext.state.user && policyContext.state.user.role.name == userRole;
 
   if (isEligible) {
     return true;
